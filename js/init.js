760e2d676a4da8b5abe7ed1f3a0a2deeb074a8a2
johnnyfyve <johnnyfyve@gmail.com>
2013-10-16 21:49:55 -0400

    updating additional pages

--- js/init.js
+++ js/init.js
@@ -60,7 +60,7 @@
 						navPanel: {
 							breakpoints: 'mobile',
 							position: 'top',
-							size: '35%',
+							size: '45%',
 							html: '<div data-action="navList" data-args="nav"></div>'
 						}
 					},
