dc7801ad3419d3aaab6e9359d6b525ecc54fb477
johnnyfyve <johnnyfyve@gmail.com>
2013-10-27 16:10:07 -0400

    added contact form and truncated navigation

---
+++ process-form.php
@@ -0,0 +1,23 @@
+<?php
+// *** Form processing code
+
+// *** First get variables
+$requestName = $_POST["name"];
+$requestEmail = $_POST["email"];  
+$requestMessage = $_POST["message"];
+
+// *** Then assemble email
+$emailMessage = "Dear SCAVMedallion.com Site Admin:\n\nThe following message was sent through the SCAVMedallion.com contact form:\n\nFrom:  ".$requestName."\nEmail: ".$requestEmail."\nMessage:\n\n".wordwrap($requestMessage, 70, "\r\n")."\n\nSCAVMedallion.com";
+
+$headers = 'From: info@scavmedallion.com' . "\r\n" .
+    'Reply-To: info@scavmedallion.com' . "\r\n" .
+    'CC:johnnyfyve@gmail.com';
+
+// *** Send email to recipients
+mail('johnnyfyve@gmail.com', 'SCAVMedallion.com site contact request', $emailMessage, $headers);
+
+// *** Create repsonse message for form page
+//echo('Thank you - your request has been sent.'.$requestName.', '.$requestEmail.', '.$requestMessage);
+echo('Thank you - your request has been sent.');
+
+?>
