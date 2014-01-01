30e9cae78877767dbfce55714e1551718a61e4ab
johnnyfyve <johnnyfyve@gmail.com>
2014-01-01 18:23:25 -0500

    updated contact form and process page

--- process-form.php
+++ process-form.php
@@ -9,12 +9,12 @@ $requestMessage = $_POST["message"];
 // *** Then assemble email
 $emailMessage = "Dear SCAVMedallion.com Site Admin:\n\nThe following message was sent through the SCAVMedallion.com contact form:\n\nFrom:  ".$requestName."\nEmail: ".$requestEmail."\nMessage:\n\n".wordwrap($requestMessage, 70, "\r\n")."\n\nSCAVMedallion.com";
 
-$headers = 'From: info@scavmedallion.com' . "\r\n" .
-    'Reply-To: info@scavmedallion.com' . "\r\n" .
+$headers = 'From: carolinaaviationmedallion@gmail.com' . "\r\n" .
+    'Reply-To: carolinaaviationmedallion@gmail.com' . "\r\n" .
     'CC:johnnyfyve@gmail.com';
 
 // *** Send email to recipients
-mail('johnnyfyve@gmail.com', 'SCAVMedallion.com site contact request', $emailMessage, $headers);
+mail('johnnyfyve@gmail.com, carolinaaviationmedallion@gmail.com', 'SCAVMedallion.com site contact request', $emailMessage, $headers);
 
 // *** Create repsonse message for form page
 //echo('Thank you - your request has been sent.'.$requestName.', '.$requestEmail.', '.$requestMessage);
